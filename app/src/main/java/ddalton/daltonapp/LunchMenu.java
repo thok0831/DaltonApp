9296ba1d2ae52bc057a0a2dfb1d27bf8b8a51a3b
Taehoon Ok <thok0831@gmail.com>
2017-02-09 08:53:27 +0900

    Updated Parsing

--- app/src/main/java/ddalton/daltonapp/LunchMenu.java
+++ app/src/main/java/ddalton/daltonapp/LunchMenu.java
@@ -3,9 +3,8 @@ package ddalton.daltonapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class LunchMenu extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -105,3 +105,4 @@ public class LunchMenu extends AppCompatActivity
         return true;
     }
 }
+
